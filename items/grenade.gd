1bafbb2e0a4ccff6d898079b05c70e61e8304f0b
Javier Araújo <javi_3838@hotmail.com>
2023-06-06 22:41:32 +0200

    Corrections were made

--- items/grenade.gd
+++ items/grenade.gd
@@ -1,19 +1,19 @@
 extends RigidBody2D
 
 
-@onready var explosion = $ExplosionAnimation
-@onready var grenade=$GrenadeSprite
+@onready var explosion:GPUParticles2D = $ExplosionParticles
+@onready var grenade:Sprite2D = $GrenadeSprite
 
 
 
 func explode() -> void:
 	print("Explotó la wea")
-	explosion.emitting=true
-	grenade.visible=false
+	explosion.emitting = true
+	grenade.visible = false
 	await get_tree().create_timer(1).timeout
 	print("Se fue a la wea")
 	queue_free()
-func _on_explosion_timer_timeout():
+func _on_explosion_timer_timeout() ->void:
 	explode()
 
 
