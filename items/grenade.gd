1333c9b2328c5ee0b084402a4df6ab8c38290662
Javier Araújo <javi_3838@hotmail.com>
2023-06-06 17:46:57 +0200

    Grenado explosion added

--- items/grenade.gd
+++ items/grenade.gd
@@ -1,12 +1,19 @@
 extends RigidBody2D
 
 
+@onready var explosion = $ExplosionAnimation
+@onready var grenade=$GrenadeSprite
 
 
 
 func explode() -> void:
 	print("Explotó la wea")
-
-
+	explosion.emitting=true
+	grenade.visible=false
+	await get_tree().create_timer(1).timeout
+	print("Se fue a la wea")
+	queue_free()
 func _on_explosion_timer_timeout():
 	explode()
+
+
