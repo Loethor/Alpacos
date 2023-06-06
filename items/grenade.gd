2276ea0fb0ef4657d2d5fbe5681c5023bbfe55a0
loethor <lrb2392@gmail.com>
2023-06-06 23:39:39 +0200

    First-born care level names

--- items/grenade.gd
+++ items/grenade.gd
@@ -1,18 +1,19 @@
 extends RigidBody2D
 
 
-@onready var explosion:GPUParticles2D = $ExplosionParticles
-@onready var grenade:Sprite2D = $GrenadeSprite
+@onready var explosion_particles: GPUParticles2D = $ExplosionParticles
+@onready var grenade_sprite: Sprite2D = $GrenadeSprite
 
 
 
 func explode() -> void:
 	print("Explotó la wea")
-	explosion.emitting = true
-	grenade.visible = false
+	explosion_particles.emitting = true
+	grenade_sprite.visible = false
 	await get_tree().create_timer(1).timeout
 	print("Se fue a la wea")
 	queue_free()
+
 func _on_explosion_timer_timeout() ->void:
 	explode()
 
