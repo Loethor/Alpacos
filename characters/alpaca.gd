19f6ed60ffe4f62e77436627003fd9cf28094bc6
loethor <lrb2392@gmail.com>
2023-06-07 22:15:09 +0200

    Added aim feature

--- characters/alpaca.gd
+++ characters/alpaca.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+enum DIRECTION {LEFT, RIGHT}
 
 const MOVEMENT_SPEED = 100.0
 const AIR_FRICTION = 1.0
@@ -15,13 +16,19 @@ const AIM_SPRITE_DISTANCE = 50
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-var facing_direction: float = -1.0
+var previous_facing_direction: DIRECTION = DIRECTION.LEFT
+var facing_direction: DIRECTION = DIRECTION.LEFT
 
-@onready var pivot: Node2D = $Pivot
+@onready var selected_weapon: Node2D = $SelectedWeapon
 @onready var alpaco_sprite:Sprite2D = $AlpacoSprite
+@onready var aim_sprite:Sprite2D = $AimSprite
+@onready var grenade_scene:PackedScene = preload("res://items/grenade.tscn")
+
+func _ready() -> void:
+
+	selected_weapon.add_child()
 
 func _physics_process(delta):
-	pivot.get_child(0).position.x = AIM_SPRITE_DISTANCE if facing_direction == 1.0 else -AIM_SPRITE_DISTANCE
 
 
 	# Add the gravity and the air friction.
@@ -29,59 +36,77 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 		velocity.x = move_toward(velocity.x, 0, AIR_FRICTION)
 
-	# Handle use.
-	if Input.is_action_just_pressed("use"):
-		use()
+	# All the actions that can be performed on the exclusively on the floor
+	if is_on_floor():
 
-	# Handle forward jump.
-	if Input.is_action_just_pressed("forward_jump") and is_on_floor():
-		velocity.y = FORWARD_JUMP_VELOCITY_Y
-		velocity.x = FORWARD_JUMP_VELOCITY_X if facing_direction == 1.0 else -FORWARD_JUMP_VELOCITY_X
-		move_and_slide()
+		# Handle forward jump.
+		if Input.is_action_just_pressed("forward_jump"):
+			velocity.y = FORWARD_JUMP_VELOCITY_Y
+			velocity.x = FORWARD_JUMP_VELOCITY_X if facing_direction == DIRECTION.RIGHT else -FORWARD_JUMP_VELOCITY_X
+			move_and_slide()
 
-	# Handle in-place jump.
-	if Input.is_action_just_pressed("in_place_jump") and is_on_floor():
-		velocity.y = IN_PLACE_JUMP_VELOCITY_Y
-		move_and_slide()
+		# Handle in-place jump.
+		if Input.is_action_just_pressed("in_place_jump"):
+			velocity.y = IN_PLACE_JUMP_VELOCITY_Y
+			move_and_slide()
 
-	# Handle flip jump.
-	if Input.is_action_just_pressed("flip_jump") and is_on_floor():
-		velocity.y = FLIP_JUMP_VELOCITY_Y
-		velocity.x = FLIP_JUMP_VELOCITY_X if facing_direction == -1.0 else -FLIP_JUMP_VELOCITY_X
-		move_and_slide()
+		# Handle flip jump.
+		if Input.is_action_just_pressed("flip_jump"):
+			velocity.y = FLIP_JUMP_VELOCITY_Y
+			velocity.x = FLIP_JUMP_VELOCITY_X if facing_direction == DIRECTION.RIGHT else -FLIP_JUMP_VELOCITY_X
+			move_and_slide()
 
-	# Handle inventory.
-	if Input.is_action_just_pressed("inventory") and is_on_floor():
-		open_inventory()
+		# Handle inventory.
+		if Input.is_action_just_pressed("inventory"):
+			open_inventory()
+
+		# Get the input direction and handle the movement/deceleration.
+
+		var direction = Input.get_axis("left", "right")
+		if direction:
+			# Store previous facing direction
+			previous_facing_direction = facing_direction
+
+			# Update facing direction
+			facing_direction = DIRECTION.RIGHT if direction == 1.0 else DIRECTION.LEFT
+
+			# Adjust aim rotation if direction has changed
+			if previous_facing_direction != facing_direction:
+				# Angle must be mirrowed in x-axis
+				aim_sprite.rotation_degrees -= 2*aim_sprite.rotation_degrees
+
+			# Update velocity
+			velocity.x = direction * MOVEMENT_SPEED
+
+			# Handle sprite direction
+			alpaco_sprite.flip_h = facing_direction == DIRECTION.RIGHT
+		else:
+			# no input, velocity set to 0
+			velocity.x = 0
+
+		# Handle Aim.
+		aim_sprite.offset.x = AIM_SPRITE_DISTANCE if facing_direction == DIRECTION.RIGHT else -AIM_SPRITE_DISTANCE
+		var aim_direction = Input.get_axis("aim_down", "aim_up")
+		var new_rotation = aim_sprite.rotation_degrees
+
+		match facing_direction:
+			DIRECTION.RIGHT:
+				new_rotation -= aim_direction
+			DIRECTION.LEFT:
+				new_rotation += aim_direction
+
+		aim_sprite.rotation_degrees = clamp(new_rotation, -90, 90)
+
 
 	# Handle menu.
 	if Input.is_action_just_pressed("menu"):
 		open_menu()
 
-	# Handle Aim.
-	var aim_direction = Input.get_axis("aim_down", "aim_up")
-	if is_on_floor():
-		# Aiming up.
-		var new_rotation = pivot.rotation_degrees
-
-		if aim_direction > 0:
-			new_rotation -= 1 * facing_direction
-		elif aim_direction < 0:
-			new_rotation += 1 * facing_direction
-
-		pivot.rotation_degrees = clamp(new_rotation, -90, 90)
+	# Handle use.
+	if Input.is_action_just_pressed("use"):
+		use()
 
 
-	# Get the input direction and handle the movement/deceleration.
-	var direction = Input.get_axis("left", "right")
-	if is_on_floor():
-		if direction:
-			velocity.x = direction * MOVEMENT_SPEED
-			facing_direction = direction
-		else:
-			velocity.x = 0
-
-	alpaco_sprite.flip_h = true if facing_direction == 1.0 else false
 	move_and_slide()
 
 func use():
