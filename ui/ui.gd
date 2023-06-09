f9de3025a0d61963f52e5ff393ab40e8de6d91f8
loethor <lrb2392@gmail.com>
2023-06-09 19:18:02 +0200

    Added wind UI scene

---
+++ ui/ui.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+@onready var wind_left_bar := $WindLeftBar
+@onready var wind_right_bar := $WindRightBar
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
